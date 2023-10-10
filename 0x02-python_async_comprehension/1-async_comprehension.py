463d29c4540a7ff65216b85c0ae4ce8b063808f7
kwamboka3 <pkwamboka@flag42.com>
2023-10-10 15:52:52 +0300

    1-async_comprehension.py

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -1,18 +1,14 @@
 #!/usr/bin/env python3
-'''Task 2's module.
+'''Task 1's module.
 '''
-import asyncio
-import time
+from typing import List
 from importlib import import_module as using
 
 
-async_comprehension = using('1-async_comprehension').async_comprehension
+async_generator = using('0-async_generator').async_generator
 
 
-async def measure_runtime() -> float:
-    '''Executes async_comprehension 4 times and measures the
-    total execution time.
+async def async_comprehension() -> List[float]:
+    '''Creates a list of 10 numbers from a 10-number generator.
     '''
-    start_time = time.time()
-    await asyncio.gather(*(async_comprehension() for _ in range(4)))
-    return time.time() - start_time
+    return [num async for num in async_generator()]
